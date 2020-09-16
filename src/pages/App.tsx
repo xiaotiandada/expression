57415f767a86ab0b1d718ec7882fbf92b72edb09
xiaotian <952822399@qq.com>
2020-09-17 01:38:19 +0800

    init

---
+++ src/pages/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './App.css';
+import Home from "./Home";
+
+
+function App() {
+  return (
+    <Home></Home>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './index.css'
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="home">
+      <header className="home-header"></header>
+      <section className="home-body">
+        <section className="box">
+          <img src="https://lh3.googleusercontent.com/proxy/-74wMApjam5zLYCwTqmlHVmekGnOIwHhb1xLAjXiI_YhqP9Rjk0t_-E69YcTs0AfnFAT5pyIDWPcxU30Py_wz3iMKB6mBzfPMbM" alt=""/>
+        </section>
+        <section className="box">
+          <img src="https://lh3.googleusercontent.com/proxy/-74wMApjam5zLYCwTqmlHVmekGnOIwHhb1xLAjXiI_YhqP9Rjk0t_-E69YcTs0AfnFAT5pyIDWPcxU30Py_wz3iMKB6mBzfPMbM" alt=""/>
+        </section>
+        <section className="box">
+          <img src="https://lh3.googleusercontent.com/proxy/-74wMApjam5zLYCwTqmlHVmekGnOIwHhb1xLAjXiI_YhqP9Rjk0t_-E69YcTs0AfnFAT5pyIDWPcxU30Py_wz3iMKB6mBzfPMbM" alt=""/>
+        </section>
+        <section className="box">
+          <img src="https://lh3.googleusercontent.com/proxy/-74wMApjam5zLYCwTqmlHVmekGnOIwHhb1xLAjXiI_YhqP9Rjk0t_-E69YcTs0AfnFAT5pyIDWPcxU30Py_wz3iMKB6mBzfPMbM" alt=""/>
+        </section>
+        <section className="box">
+          <img src="https://lh3.googleusercontent.com/proxy/-74wMApjam5zLYCwTqmlHVmekGnOIwHhb1xLAjXiI_YhqP9Rjk0t_-E69YcTs0AfnFAT5pyIDWPcxU30Py_wz3iMKB6mBzfPMbM" alt=""/>
+        </section>
+        <section className="box">
+          <img src="https://lh3.googleusercontent.com/proxy/-74wMApjam5zLYCwTqmlHVmekGnOIwHhb1xLAjXiI_YhqP9Rjk0t_-E69YcTs0AfnFAT5pyIDWPcxU30Py_wz3iMKB6mBzfPMbM" alt=""/>
+        </section>
+        <section className="box">
+          <img src="https://lh3.googleusercontent.com/proxy/-74wMApjam5zLYCwTqmlHVmekGnOIwHhb1xLAjXiI_YhqP9Rjk0t_-E69YcTs0AfnFAT5pyIDWPcxU30Py_wz3iMKB6mBzfPMbM" alt=""/>
+        </section>
+        <section className="box">
+          <img src="https://lh3.googleusercontent.com/proxy/-74wMApjam5zLYCwTqmlHVmekGnOIwHhb1xLAjXiI_YhqP9Rjk0t_-E69YcTs0AfnFAT5pyIDWPcxU30Py_wz3iMKB6mBzfPMbM" alt=""/>
+        </section>
+        <section className="box">
+          <img src="https://lh3.googleusercontent.com/proxy/-74wMApjam5zLYCwTqmlHVmekGnOIwHhb1xLAjXiI_YhqP9Rjk0t_-E69YcTs0AfnFAT5pyIDWPcxU30Py_wz3iMKB6mBzfPMbM" alt=""/>
+        </section>
+        <section className="box">
+          <img src="https://lh3.googleusercontent.com/proxy/-74wMApjam5zLYCwTqmlHVmekGnOIwHhb1xLAjXiI_YhqP9Rjk0t_-E69YcTs0AfnFAT5pyIDWPcxU30Py_wz3iMKB6mBzfPMbM" alt=""/>
+        </section>
+      </section>
+    </div>
+  );
+}
+
+export default App;

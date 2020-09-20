21cb9b2852dd3e7b33d5fcca4454a609669d8c93
xiaotian <952822399@qq.com>
2020-09-20 23:35:05 +0800

    scrollmagic

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -4,3 +4,8 @@ declare module 'react-waterfall-h5' {
   import Waterfall from 'react-waterfall-h5';
   export default Waterfall
 }
+
+declare module 'scrollmagic' {
+  import ScrollMagic from 'scrollmagic'
+  export default ScrollMagic
+}
